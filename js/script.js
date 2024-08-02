1c7f54cce3e1dc1d60dad8cbe977988d868263d0
k0r0lina <karolina.rzasa2002@gmail.com>
2024-08-02 11:19:28 +0200

    Add titleClickHandler for left sidebar title links

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function titleClickHandler(event) {
+  console.log(event);
+}
+
+const links = document.querySelectorAll(".titles a");
+
+for (let link of links) {
+  link.addEventListener("click", titleClickHandler);
+}
